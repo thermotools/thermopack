01a868cd1e520d09c953c0878d23e54f85d9808c
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2022-04-06 14:06:13 +0200

    Removed comment from when i was writing the file

--- addon/pycThermopack/makescript.py
+++ addon/pycThermopack/makescript.py
@@ -36,15 +36,3 @@ with open('pyctp/platform_specifics.py', 'w') as file:
         file.write(tab+'pf_specifics["'+k+'"] = "'+v+'"\n')
     
     file.write(tab+'return pf_specifics')
-
-'''
-def get_platform_specifics():
-    pf_specifics = {}
-    pf_specifics["prefix"] = "__"
-    pf_specifics["module"] = "_MOD_"
-    pf_specifics["postfix"] = ""
-    pf_specifics["postfix_no_module"] = "_"
-    pf_specifics["dyn_lib"] = "libthermopack.so"
-    pf_specifics["os_id"] = "linux"
-    return pf_specifics
-'''
