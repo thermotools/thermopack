70ae84747b07696d5fc4cf3aeee5ec5aaf8f8715
vegardjervell <vegard.g.j@icloud.com>
2022-05-13 18:23:33 +0200

    Added platform_specifics.py to gitignore

--- addon/pycThermopack/pyctp/platform_specifics.py
+++ addon/pycThermopack/pyctp/platform_specifics.py
@@ -1,6 +1,13 @@
-# Module for platform specific stuff.
-from . import map_platform_specifics
+# Module for platform specific stuff. Automatically generated.
+# Timestamp : 2022-05-13T18:08:45.309273
 
 
 def get_platform_specifics():
-    return map_platform_specifics.get_platform_specifics_from_platform()
+    pf_specifics = {}
+    pf_specifics["os_id"] = "darwin"
+    pf_specifics["prefix"] = "__"
+    pf_specifics["module"] = "_MOD_"
+    pf_specifics["postfix"] = ""
+    pf_specifics["postfix_no_module"] = "_"
+    pf_specifics["dyn_lib"] = "libthermopack.dynlib"
+    return pf_specifics
