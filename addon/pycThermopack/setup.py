2df80ba1d984832619a0e252c9d8bec315ec343d
Morten Hammer <morten.hammer@ntnu.no>
2022-10-28 20:28:34 +0200

    Added pyproject.toml file

--- addon/pycThermopack/setup.py
+++ addon/pycThermopack/setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
+import sys
+
+sys.argv.extend(['--plat-name', 'linux_x86_64'])
+#sys.argv.extend(['--plat-name', 'win_amd64'])
 
 setup(name='pyctp',
-      version='1.0.0',
+      version='2.0.0',
       description='pycThermopack',
       author='Morten Hammer',
       author_email='hammer.morten@sintef.no',
-      url='https://github.com/SINTEF/thermopack',
+      url='https://github.com/thermotools/thermopack',
       packages=['pyctp'],
-      package_data={'pyctp':['libthermopack.*']}
+      package_data={'pyctp':['*thermopack.*']}
 )
 
