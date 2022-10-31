908aeb4a7a2e95a417561f6904d9ea5e4e59c4be
Morten Hammer <morten.hammer@ntnu.no>
2022-10-31 14:29:08 +0100

    Setup working for both windows and manylinux wheel generation

---
+++ addon/pycThermopack/README_pypi.md
@@ -0,0 +1,3 @@
+# Python interface for Thermopack using ctypes (pyctp)
+
+To get started take a look at the example code provided with the github source code. Also the function headers of the EoS classes, as well as the base class `thermo.py`, will help you understand what Thermopack can do.
--- addon/pycThermopack/setup.py
+++ addon/pycThermopack/setup.py
@@ -1,14 +1,12 @@
 from distutils.core import setup
 import sys
 
-sys.argv.extend(['--plat-name', 'linux_x86_64'])
-#sys.argv.extend(['--plat-name', 'win_amd64'])
 
 setup(name='pyctp',
       version='2.0.0',
-      description='pycThermopack',
+      description='Python interface to thermopack',
       author='Morten Hammer',
-      author_email='hammer.morten@sintef.no',
+      author_email='morten.hammer@sintef.no',
       url='https://github.com/thermotools/thermopack',
       packages=['pyctp'],
       package_data={'pyctp':['*thermopack.*']}
