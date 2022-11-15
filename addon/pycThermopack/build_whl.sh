367e03bd08de731d908be5a39adf2586f723cb39
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2022-11-15 14:09:39 +0100

    Automated building of macos x86 wheel

---
+++ addon/pycThermopack/build_whl.sh
@@ -0,0 +1,11 @@
+cd ~/thermopack/
+# make clean
+# make optim
+cd addon/pycThermopack
+python makescript.py optim
+python map_platform_specifics.py
+python -m pip wheel --wheel-dir=wheelhouse .
+cd wheelhouse
+delocate-wheel -w fixed_wheels -v thermopack-2.0.0-py3-none-any.whl
+cd fixed_wheels
+mv thermopack-2.0.0-py3-none-any.whl thermopack-2.0.0-py3-none-macosx_10_9_x86_64.whl
