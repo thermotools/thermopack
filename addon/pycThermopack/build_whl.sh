01efe77c89e35785ec8ffb5e42854506168fc894
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2022-11-15 11:58:52 +0100

    Automated wheel building process (nb: version number must be manually updated)

---
+++ addon/pycThermopack/build_whl.sh
@@ -0,0 +1,13 @@
+cd ~/thermopack/
+make clean
+make optim
+cd addon/pycThermopack
+python makescript.py optim
+python map_platform_specifics.py
+python -m pip wheel --wheel-dir=wheelhouse .
+cd wheelhouse
+delocate-wheel -w fixed_wheels -v thermopack-2.0.0-py3-none-any.whl
+cd fixed_wheels
+mv thermopack-2.0.0-py3-none-any.whl thermopack-2.0.0-py3-none-macosx_11_0_arm64.whl
+cd ..
+rm thermopack-2.0.0-py3-none-any.whl # Prevent using the old wheel by accident if version is updated, but build script is not
