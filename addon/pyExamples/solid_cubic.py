725023a3668842bd2464f3439f43514d1b16668b
Tage Maltby <tage.maltby@ntnu.no>
2022-12-02 14:36:56 +0100

    Added Temperature density envelope for mixture

--- addon/pyExamples/solid_cubic.py
+++ addon/pyExamples/solid_cubic.py
@@ -12,8 +12,9 @@ import matplotlib.pyplot as plt
 # Instanciate and init cubic object
 cb = cubic("CO2,N2", "PR", "HV", "Classic")
 cb.init_solid("CO2")
-z = np.array([0.98, 0.02])
-lines, crits, triples = cb.solid_envelope_plot(1.0e5, z)
+cb.set_pmax(5.0e9)
+z = np.array([0.98,0.02])
+lines, crits, triples = cb.solid_envelope_plot(1.0e5, z, calc_esv = True, maximum_pressure=1.0e9)
 p_scaling = 1.0e-6
 plt.figure()
 for i in range(len(lines)):
@@ -25,9 +26,30 @@ for i in range(len(crits)):
     label = None
 leg = plt.legend(loc="best", numpoints=1)
 leg.get_frame().set_linewidth(0.0)
+plt.ylim([0.0, 15.0])
 plt.ylabel(r"$P$ (MPa)")
 plt.xlabel(r"$T$ (K)")
 plt.title("Solid-gas-liquid phase diagram")
+plt.figure()
+
+# Plotting phase diagram for temperatures and density
+n_scaling = 0.001# m3 to Liters
+for i in range(len(lines)):
+    densities = n_scaling/lines[i][:,4]
+    plt.plot(densities,lines[i][:,0])
+
+# Critical point
+label = "Critical point"
+for i in range(len(crits)):
+    plt.plot(n_scaling/crits[i][4], crits[i][0], linestyle="None",
+             marker="o", color="k", label=label)
+    label = None
+
+plt.ylabel(r"$T$ (K)")
+plt.xlabel(r"$\rho$ (mol/L)")
+plt.title("CO2,N2 solid-gas-liquid phase diagram")
+leg = plt.legend(loc="best", numpoints=1, frameon=False)
+
 
 # Instanciate and init cubic object
 cb = cubic("CO2", "PR", "HV", "Classic")
