b1e9efbc52f49857bdc55240da25b590c70a61b3
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2022-12-06 10:30:04 +0100

    Added readme to pypi long_description
    Copy-pasted the readme in the root-dir, but removed the setup and file structure sections. Fixed a typo in the root dir readme.

--- addon/pycThermopack/setup.py
+++ addon/pycThermopack/setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
+from pathlib import Path
 import sys
 
-
+root_dir = Path(__file__).parent # thermopack root directory
+readme = (root_dir / 'README_pypi.md').read_text()
 setup(name='thermopack',
       version='2.0.0',
       description='Python interface to thermopack',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       author='Morten Hammer',
       author_email='morten.hammer@sintef.no',
       url='https://github.com/thermotools/thermopack',
