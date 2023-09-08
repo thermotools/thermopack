5ec806ab687893700ec381c74473e08059f191ee
Morten Hammer <morten.hammer@ntnu.no>
2023-09-08 09:44:32 +0200

    Added nested loop search for criconden-bar/therm. Added option for criconden-bar/therm in python interface.

--- addon/pyExamples/ext_csp.py
+++ addon/pyExamples/ext_csp.py
@@ -14,10 +14,12 @@ csp = ext_csp("C1,C2,C3,NC4", "SRK", "Classic", "vdW", "NIST_MEOS", "C3")
 
 # Plot phase envelope
 z = np.array([0.86, 0.1, 0.03, 0.01])
-T, P = csp.get_envelope_twophase(1.0e4, z, maximum_pressure=1.5e7)
+T, P, criconden = csp.get_envelope_twophase(1.0e4, z, maximum_pressure=1.5e7, calc_criconden=True)
 Tc, vc, Pc = csp.critical(z)
 plt.plot(T, P*1.0e-6)
 plt.plot([Tc], [Pc*1.0e-6], "ko")
+plt.plot(criconden[0], criconden[1]*1.0e-6, "go")
+plt.plot(criconden[2], criconden[3]*1.0e-6, "ro")
 plt.ylabel(r"$P$ (MPa)")
 plt.xlabel(r"$T$ (K)")
 plt.title("Extended corrensponding state phase diagram")
