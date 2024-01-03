8cc1bf541ea4b298c7e909c76d78e75453dd8706
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-01-03 10:33:33 +0100

    Removed unnessecary <if x is not None> checks for binary_pxy plot

--- addon/pyExamples/cpa.py
+++ addon/pyExamples/cpa.py
@@ -25,21 +25,19 @@ ax2.tick_params(labelright='off')
 # Pxy phase envelope
 p_scaling = 1.0e-6
 lle, l1ve, l2ve = cpa_srk.get_binary_pxy(298.1)
-if lle[0] is not None:
-    ax.plot(lle.x1, lle.p * p_scaling)
-    ax.plot(lle.x2, lle.p * p_scaling)
-    ax2.plot(lle.x1, lle.p * p_scaling)
-    ax2.plot(lle.x2, lle.p * p_scaling)
-if l1ve[0] is not None:
-    ax.plot(l1ve.x, l1ve.p * p_scaling)
-    ax.plot(l1ve.y, l1ve.p * p_scaling)
-    ax2.plot(l1ve.x, l1ve.p * p_scaling)
-    ax2.plot(l1ve.y, l1ve.p * p_scaling)
-if l2ve[0] is not None:
-    ax.plot(l2ve.x, l2ve.p * p_scaling)
-    ax.plot(l2ve.y, l2ve.p * p_scaling)
-    ax2.plot(l2ve.x, l2ve.p * p_scaling)
-    ax2.plot(l2ve.y, l2ve.p * p_scaling)
+
+# Plotting Liquid - Liquid equilibria
+ax.plot(lle.x1, lle.p * p_scaling)
+ax.plot(lle.x2, lle.p * p_scaling)
+ax2.plot(lle.x1, lle.p * p_scaling)
+ax2.plot(lle.x2, lle.p * p_scaling)
+
+# Plotting Liquid - Vapour equilibria
+for lve in (l1ve, l2ve):
+    ax.plot(lve.x, lve.p * p_scaling)
+    ax.plot(lve.y, lve.p * p_scaling)
+    ax2.plot(lve.x, lve.p * p_scaling)
+    ax2.plot(lve.y, lve.p * p_scaling)
 
 d = .015
 kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
