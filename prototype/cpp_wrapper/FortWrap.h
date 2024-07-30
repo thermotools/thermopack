616d686345850b9c27c2f739b3389da2ac7e11bf
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-07-30 17:12:10 +0200

    Delete old fortwrap-based c++ prototype wrapper.

--- prototype/cpp_wrapper/FortWrap.h
+++
@@ -1,9 +0,0 @@
-/* This source file automatically generated on 2023-06-07 using 
-   FortWrap wrapper generator version 2.2.2 */
-
-#include "VariantEoS.h"
-#include "Variant1.h"
-#include "Variant2.h"
-#include "NotIdGas.h"
-#include "BaseEos.h"
-
--- prototype/rebuild_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-g++-12 -c main.cpp
-g++-12 -o main main.o wrappers/*.o src/*.o /opt/homebrew/lib/gcc/12/libgfortran.dylib
-./main
