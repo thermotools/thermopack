20940ce371782aadb4b7b799528ba400f91cfc69
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-08-09 11:40:24 +0200

    Add Lapack@v3.12.0 as submodule, and modify cppExamples to leverage find_package.
    Also add some status messages in thermopack-config.cmake. The build on windows will probably need some testing/tuning.

--- thermopack-config.cmake
+++ thermopack-config.cmake
@@ -7,12 +7,14 @@
 #   thermopack : Imported shared library target with headers
 
 
+message(STATUS "ThermoPack found at: ${CMAKE_CURRENT_LIST_DIR}")
 set(THERMOPACK_LIB ${CMAKE_CURRENT_LIST_DIR}/addon/cppThermopack/libthermopack${CMAKE_SHARED_LIBRARY_SUFFIX})
 set(THERMOPACK_ROOT ${CMAKE_CURRENT_LIST_DIR})
 
 if(NOT EXISTS ${THERMOPACK_LIB})
     unset(THERMOPACK_LIB)
     set(THERMOPACK_INSTALLED FALSE)
+    message(STATUS "ThermoPack has not been properly installed.")
     return()
 endif()
 
@@ -20,9 +22,15 @@ set(THERMOPACK_FOUND TRUE)
 set(THERMOPACK_INSTALLED TRUE)
 set(THERMOPACK_INCLUDE ${CMAKE_CURRENT_LIST_DIR}/addon/cppThermopack)
 
+message(STATUS "Setting ThermoPack cmake variables ...")
+message(STATUS "THERMOPACK_ROOT: ${THERMOPACK_ROOT}")
+message(STATUS "THERMOPACK_LIB: ${THERMOPACK_LIB}")
+message(STATUS "THERMOPACK_INCLUDE: ${THERMOPACK_INCLUDE}")
+
 if(NOT TARGET thermopack)
     add_library(thermopack SHARED IMPORTED)
     set_target_properties(thermopack PROPERTIES 
                             IMPORTED_LOCATION ${THERMOPACK_LIB}
                             INTERFACE_INCLUDE_DIRECTORIES ${THERMOPACK_INCLUDE})
+    message(STATUS "ThermoPack exported target: thermopack")
 endif()
