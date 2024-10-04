4efc4aab22ed1b1f2c55b6e09a089dae36ecd781
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-10-04 16:39:06 +0200

    Improve tp_property example

--- addon/cppExamples/tp_properties.cpp
+++ addon/cppExamples/tp_properties.cpp
@@ -1,3 +1,16 @@
+/*
+    Demonstrate computation of properties in the TP-interface, i.e. functions of (T, p, z, phase).
+    
+    Note that the `Property` class has an implicit conversion to `double`, so if no derivatives are needed, 
+    a `Property` can be returned to a `double` (as demonstrated for specific_volume). A property can also be
+    `static_cast<double>`'d, as demonstrated for enthalpy. 
+
+    Otherwise the `value` of a property can be extracted with the Property::value() method.
+
+    Derivative flags (the booleans in the example) are given in the order
+        dt, dp, dn
+    with the default value `false`.
+*/
 #include <cppThermopack/saftvrmie.h>
 #include <iostream>
 #include <vector>
@@ -11,19 +24,25 @@ int main(){
     std::vector<double> z = {0.7, 0.3};
 
     double v = svrm.specific_volume(T, p, z, svrm.VAPPH);
-    std::cout << "Vapour phase Specific volume is : " << v << " m3 / mol\n";
+    std::cout << "Vapour phase Specific volume : " << v << " m3 / mol\n\n";
 
-    Property rho = svrm.molar_density(T, p, z, svrm.LIQPH, true);
-    std::cout << "Liquid phase density derivative wrt. temperature (drho / dT) : " << rho.dt() << " mol / m3 K\n";
+    double density = svrm.molar_density(T, p, z, svrm.LIQPH); // No need for `Property` - we dont want derivatives
+    std::cout << "Liquid phase density : " << density << " mol / m3\n";
+    Property rho = svrm.molar_density(T, p, z, svrm.LIQPH, true); // Casting to `double` will discard the derivative, so here we need `Property`
+    std::cout << "Liquid phase density derivative wrt. temperature (drho / dT) : " << rho.dt() << " mol / m3 K\n\n";
 
-    Property h = svrm.enthalpy(T, p, z, svrm.VAPPH, true);
-    std::cout << "Vapour phase, specific enthalpy : " << static_cast<double>(h) << " J / mol, " << "Heat capacity : " << h.dt() << " J / mol K\n";
-    std::cout << "Liquid phase, specific enthalpy : " << static_cast<double>(svrm.enthalpy(T, p, z, svrm.LIQPH)) << " J / mol\n";
+    Property h = svrm.enthalpy(T, p, z, svrm.VAPPH, true, false, true); // Compute enthaly and temperature and mole number derivatives
+    std::cout << "Vapour phase, specific enthalpy : " << h.value() << " J / mol\n";
+    std::cout << "Vapour phase heat capacity : "<< h.dt() << " J / mol K\n";
+    std::cout << "Vapour phase partial molar enthalpies : " << h.dn()[0] << ", " << h.dn()[1] << " J / mol\n";
+    std::cout << "Liquid phase, specific enthalpy : " << static_cast<double>(svrm.enthalpy(T, p, z, svrm.LIQPH)) << " J / mol\n\n"; // We can cast the output to a double
 
-    Property s_vap = svrm.entropy(T, p, z, svrm.VAPPH, false, false, true);
-    std::cout << "Vapour phase partial molar entropies : " << s_vap.dn()[0] << ", " << s_vap.dn()[0] << " J / mol^2 K\n";
-    Property s_liq = svrm.entropy(T, p, z, svrm.LIQPH, false, false, true);
-    std::cout << "Liquid phase partial molar entropies : " << s_liq.dn()[0] << ", " << s_liq.dn()[1] << " J / mol^2 K\n";
+    Property s_vap = svrm.entropy(T, p, z, svrm.VAPPH, false, true, false); // Compute entropy and pressure derivative
+    double dsdp = s_vap.dp(); // Extract pressure derivative
+    std::cout << "Vapour phase entropy derivative wrt. pressure (ds/dp) : " << dsdp << " J / mol Pa K\n";
+    Property s_liq = svrm.entropy(T, p, z, svrm.LIQPH, false, false, true); // Compute entropy and mole number derivatives
+    std::vector<double> dsdn = s_liq.dn(); // Extract mole number derivatives
+    std::cout << "Liquid phase partial molar entropies : " << dsdn[0] << ", " << dsdn[1] << " J / mol K\n";
 
     return 0;
 }
