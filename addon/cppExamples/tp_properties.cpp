c80107ac8296a1c1ca9bea72164f453abbe55d39
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-10-04 16:15:55 +0200

    Implement and expose molar_density function.

--- addon/cppExamples/tp_properties.cpp
+++ addon/cppExamples/tp_properties.cpp
@@ -10,9 +10,15 @@ int main(){
     double p = 1e6;
     std::vector<double> z = {0.7, 0.3};
 
-    double h = svrm.enthalpy(T, p, z, svrm.VAPPH);
-    std::cout << "Vapour phase specific enthalpy : " << h << " J / mol\n";
-    std::cout << "Liquid phase specific enthalpy : " << static_cast<double>(svrm.enthalpy(T, p, z, svrm.LIQPH)) << " J / mol\n";
+    double v = svrm.specific_volume(T, p, z, svrm.VAPPH);
+    std::cout << "Vapour phase Specific volume is : " << v << " m3 / mol\n";
+
+    Property rho = svrm.molar_density(T, p, z, svrm.LIQPH, true);
+    std::cout << "Liquid phase density derivative wrt. temperature (drho / dT) : " << rho.dt() << " mol / m3 K\n";
+
+    Property h = svrm.enthalpy(T, p, z, svrm.VAPPH, true);
+    std::cout << "Vapour phase, specific enthalpy : " << static_cast<double>(h) << " J / mol, " << "Heat capacity : " << h.dt() << " J / mol K\n";
+    std::cout << "Liquid phase, specific enthalpy : " << static_cast<double>(svrm.enthalpy(T, p, z, svrm.LIQPH)) << " J / mol\n";
 
     Property s_vap = svrm.entropy(T, p, z, svrm.VAPPH, false, false, true);
     std::cout << "Vapour phase partial molar entropies : " << s_vap.dn()[0] << ", " << s_vap.dn()[0] << " J / mol^2 K\n";
