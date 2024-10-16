79cca4c1f1cd775fb43e1c2dc9f440b774707b21
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-10-16 21:09:48 +0200

    Add release of compiled lib and headers in zip format to workflow.

---
+++ .github/scripts/create_cppdist.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+platforms=("macos-latest" "macos-12" "ubuntu-latest" "windows-latest")
+dylib_dirs=(".dylibs" ".dylibs" "../thermopack.libs" "NO_DYLIB_DIR")
+
+length=${#platforms[@]}
+for ((i=0; i<$length; i++)); do
+    platform=${platforms[$i]}
+    whldir="wheel-v3-${platform}"
+    dylib_dir=${dylib_dirs[$i]}
+    if [ -d "$whldir" ]; then
+        echo "Processing directory: $whldir"
+        echo "Directory contains: "
+        ls -AR $whldir
+
+        # Create dist directory and move config- and header files to expected locations
+        distdir="thermopack-${platform}"
+        if [ "${platform}" == "windows-latest" ]; then
+            mkdir -p ${distdir}/installed
+        else
+            mkdir -p ${distdir}/installed/${dylib_dir}
+        fi
+        mkdir -p ${distdir}/addon/cppThermopack
+        cp thermopack-config.cmake ${distdir}
+        cp -r addon/cppThermopack/cppThermopack ${distdir}/addon/cppThermopack
+
+        echo "Set up dist directory ${distdir}: "
+        ls -AR $distdir
+        
+        echo "Unpacking wheel ..." 
+        cd $whldir
+        unzip thermopack*
+        cd ..
+        mv ${whldir}/thermopack/libthermopack* ${distdir}/installed
+
+        # Do not need to relocate any dylib dependencies for windows
+        if [ "${platform}" != "windows-latest" ]; then
+            mv ${whldir}/thermopack/${dylib_dir}/* ${distdir}/installed/${dylib_dir}
+        fi
+
+        echo "Completed move to dist directory ${distdir}:"
+        ls -AR ${distdir}
+
+        zip ${distdir}.zip ${distdir}
+    else
+        echo "Directory $dir does not exist."
+    fi
+done
