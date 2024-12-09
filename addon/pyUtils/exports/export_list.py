8059188d5fd8c255b888666d169d0f4819e7ed60
Vegard Gjeldvik Jervell <vegard.g.j@icloud.com>
2024-12-09 14:50:10 +0100

    Fix export list.

--- addon/pyUtils/exports/export_list.py
+++ addon/pyUtils/exports/export_list.py
@@ -188,10 +188,10 @@ append_export("fmt_energy_density", "fundamental_measure_theory")
 
 append_export("calc_bmcsl_gij_fmt", "hardsphere_bmcsl")
 
-append_export("hydrate", "init_hydrate_model")
-append_export("hydrate", "fugacity_water_in_hydrate_tpx")
-append_export("hydrate", "fugacity_water_in_hydrate_tvn")
-append_export("hydrate_curves", "map_hydrate_appearance_curve")
+append_export("init_hydrate_model", "hydrate")
+append_export("fugacity_water_in_hydrate_tpx", "hydrate")
+append_export("fugacity_water_in_hydrate_tvn", "hydrate")
+append_export("map_hydrate_appearance_curve", "hydrate_curves")
 
 append_export("set_standard_entropy", "ideal")
 append_export("get_standard_entropy", "ideal")
